2a453aeb11908f64fe4d96296f5414f827e5ccd6
Jesse <dynaptx@163.com>
2020-06-06 23:59:51 +0800

    增加png

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ var server = http.createServer(function (request, response) {
         ".html": "text/html",
         ".css": "text/css",
         ".js": "text/javascript",
+        ".png": "image/png",
         ".jpg": "image/jpeg",
     };
     response.setHeader(
